09125d4e8e93a24f9ae07bc5c6b8c6f2f0fb8ff9
yeasinmiah1272001 <137316520+yeasinmiah1272001@users.noreply.github.com>
2025-03-18 16:39:53 +0600

    cors add

--- index.js
+++ index.js
@@ -8,8 +8,13 @@ const jwt = require("jsonwebtoken");
 
 const port = process.env.PORT || 8000;
 
+const corsOptions = {
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 // middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
